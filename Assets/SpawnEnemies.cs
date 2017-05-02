8e70c59c80cecc95965ac27af7346de84444206d
daniwp <danielwinkel4@hotmail.com>
2017-05-02 11:24:40 +0200

    heeelp

---
+++ Assets/SpawnEnemies.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour {
+
+    public GameObject enemy;
+    public GameObject boss;
+    public List<GameObject> spawnPoints;
+    public GameObject spawnPointBoss;
+    public float spawnTime;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("SpawnEnemy", spawnTime, spawnTime);
+        InvokeRepeating("SpawnBoss", 5, 30);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    void SpawnEnemy()
+    {
+        int r = Random.Range(0, spawnPoints.Count);
+        Instantiate(enemy, spawnPoints[r].transform.position, spawnPoints[r].transform.rotation);
+    }
+
+    void SpawnBoss()
+    {
+        Instantiate(boss, spawnPointBoss.transform.position, spawnPointBoss.transform.rotation);
+    }
+}
